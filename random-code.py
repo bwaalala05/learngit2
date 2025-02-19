f77c64f52412acf5ed8c624277a94c262fb001a8
bwaa <bwaalala14@gmail.com>
2025-02-19 10:34:53 +0800

    random-code.py

--- random-code.py
+++ random-code.py
@@ -1,4 +1,9 @@
-import numpy
-
-num=input("Enter a random number")
+num=int(input("Enter a random number"))
 print(num)
+if num>10:
+    print("wow you greedy")
+
+# ls = list out the files in the current directory
+# cd ~ = go back one step in directory
+# git log = gives a history of what we have done (Q to kill)
+# git status = display modified files
